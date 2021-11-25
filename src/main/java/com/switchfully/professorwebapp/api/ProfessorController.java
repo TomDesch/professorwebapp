2375e74c2add3e17914347c8c200b412c0e07552
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-25 18:19:30 +0100

    Created a dummy db called ProfessorRepository

--- src/main/java/com/switchfully/professorwebapp/api/ProfessorController.java
+++ src/main/java/com/switchfully/professorwebapp/api/ProfessorController.java
@@ -1,19 +1,19 @@
 package com.switchfully.professorwebapp.api;
 
 
+import com.switchfully.professorwebapp.api.dtos.CreateProfessorDto;
 import com.switchfully.professorwebapp.domain.Professor;
 import com.switchfully.professorwebapp.api.dtos.ProfessorDto;
+import com.switchfully.professorwebapp.domain.ProfessorRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path = "professors")
 public class ProfessorController {
+
 //    Make it so that we can:
 //    Create a new professor.
 //    Get all the professors.
@@ -23,7 +23,6 @@ public class ProfessorController {
 //    Create a specific service and the required domain objects to succeed in these requirements
 
 //    Try to make use of DTO's (todo and dedicated mappers to map those DTO's from and to your domain objects).
-//
 //    Furthermore, think about todo edge-cases, e.g.:
 
     private ProfessorRepository professorRepository;
@@ -35,9 +34,11 @@ public class ProfessorController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
-    public ProfessorDto createProfessor() {
-        Professor professor =
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ProfessorDto createProfessor(@RequestBody CreateProfessorDto createProfessorDto) {
+        Professor professor = professorMapper.mapToProfessor(createProfessorDto);
+        Professor savedProf = professorRepository.save(professor);
+        return professorMapper.mapToDto(savedProf);
     }
 
 }
---
+++ src/main/java/com/switchfully/professorwebapp/domain/ProfessorRepository.java
@@ -0,0 +1,32 @@
+package com.switchfully.professorwebapp.domain;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+// dummy db
+
+@Component
+public class ProfessorRepository {
+    private final ConcurrentHashMap<String, Professor> professorsById;
+
+    public ProfessorRepository() {
+        professorsById = new ConcurrentHashMap<>();
+    }
+
+    public Professor save(Professor professor) {
+        professorsById.put(professor.getId(), professor);
+        return professor;
+    }
+
+    public Professor getById(String id) throws IllegalArgumentException {
+        var foundProfessor = professorsById.get(id);
+        if (foundProfessor == null) {
+            throw new IllegalArgumentException("No professor could be found for id " + id);
+        }
+        return foundProfessor;
+    }
+
+    public Collection<Professor> getAll() {return professorsById.values();}
+}
