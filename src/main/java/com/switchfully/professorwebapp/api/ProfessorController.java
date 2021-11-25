7f7cb29f7b9b3297e93fabf505e3a0a37f40e959
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-25 17:53:52 +0100

    Created Professor

--- src/main/java/com/switchfully/professorwebapp/api/ProfessorController.java
+++ src/main/java/com/switchfully/professorwebapp/api/ProfessorController.java
@@ -1,6 +1,16 @@
 package com.switchfully.professorwebapp.api;
 
 public class ProfessorController {
+//    Make it so that we can:
+//    Create a new professor.
+//    Get all the professors.
+//    Get the professor for a given id.
+//    Update an existing professor (an id is provided) with a new set of data.
+//    Delete an existing professor for a given id.
+//    Create a specific service and the required domain objects to succeed in these requirements
 
+//    Try to make use of DTO's (todo and dedicated mappers to map those DTO's from and to your domain objects).
+//
+//    Furthermore, think about todo edge-cases, e.g.:
 
 }
--- src/main/java/com/switchfully/professorwebapp/domain/Professor.java
+++ src/main/java/com/switchfully/professorwebapp/domain/Professor.java
@@ -1,26 +1,60 @@
 package com.switchfully.professorwebapp.domain;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class Professor {
 
+    private final String id;
+    private String firstName;
+    private String lastName;
 
+    public Professor(String firstName, String lastName) {
+        this.id = UUID.randomUUID().toString();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-//    Create a controller specific for the professor resource
-//    Make it so that we can:
-//    Create a new professor.
-//    Get all the professors.
-//    Get the professor for a given id.
-//    Update an existing professor (an id is provided) with a new set of data.
-//    Delete an existing professor for a given id.
-//    Create a specific service and the required domain objects to succeed in these requirements
-//    A professor consists of an:
-//    id
-//            firstname
-//    lastname
-//    Try to make use of DTO's (and dedicated mappers to map those DTO's from and to your domain objects).
-//
-//    Furthermore, think about edge-cases, e.g.:
-//
-//    What if we try to update a professor, but could not find a professor for the provided id...?
+    public String getId() {
+        return id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Professor professor)) return false;
+        return Objects.equals(id, professor.id)
+                && Objects.equals(firstName, professor.firstName)
+                && Objects.equals(lastName, professor.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
