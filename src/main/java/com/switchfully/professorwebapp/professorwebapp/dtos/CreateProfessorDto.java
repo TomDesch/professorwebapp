466c6f3099403299851e33025b04e83675bdc1bb
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-25 17:40:26 +0100

    Made CreateProfessorDto

--- src/main/java/com/switchfully/professorwebapp/professorwebapp/dtos/CreateProfessorDto.java
+++ src/main/java/com/switchfully/professorwebapp/professorwebapp/dtos/CreateProfessorDto.java
@@ -1,4 +1,25 @@
 package com.switchfully.professorwebapp.professorwebapp.dtos;
 
 public class CreateProfessorDto {
+
+    private String firstName;
+    private int lastName;
+
+    public CreateProfessorDto setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public CreateProfessorDto setLastName(int lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
 }
