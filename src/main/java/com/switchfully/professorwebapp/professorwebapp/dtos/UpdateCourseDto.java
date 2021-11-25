8541444f4f36be2741a7f21707191fa45db24d6d
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-11-25 17:29:23 +0100

    Made UpdateCourseDto

--- src/main/java/com/switchfully/professorwebapp/professorwebapp/dtos/UpdateCourseDto.java
+++ src/main/java/com/switchfully/professorwebapp/professorwebapp/dtos/UpdateCourseDto.java
@@ -1,4 +1,39 @@
 package com.switchfully.professorwebapp.professorwebapp.dtos;
 
+import com.switchfully.professorwebapp.domain.Professor;
+
 public class UpdateCourseDto {
+
+    private String title;
+    private int studyPoints;
+    private Professor professor;
+
+    public UpdateCourseDto setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public UpdateCourseDto setStudyPoints(int studyPoints) {
+        this.studyPoints = studyPoints;
+        return this;
+    }
+
+    public UpdateCourseDto setProfessor(Professor professor) {
+        this.professor = professor;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getStudyPoints() {
+        return studyPoints;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+
 }
